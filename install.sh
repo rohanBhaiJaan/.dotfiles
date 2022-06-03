bbb0654c7de1246a8c7a832d26161871627c59d9
rohan <rohanprogrammer@gmail.com>
2022-06-03 06:24:10 +0530

    improved setup.sh and removed install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-presentDirs=(*/)
-for dir in ${presentDirs[@]}; do
-    stow -t $HOME -D $dir
-    stow -t $HOME -S $dir
-done
--- setup.sh
+++ setup.sh
@@ -1,22 +1,30 @@
-#~/bin/bash
-cd $HOME 
-install_packages="apt install neovim stow git zsh curl ctags fzf python clang tmux man tree"
+#!/bin/bash
 
-installPackages () {
-    if ! [[ "$OSTYPE" = "linux-androideabi" ]]; then
-        install_packages="$install_packages gcc g++"
-        echo $install_packages
-    fi
-    apt update && apt upgrade
-    $install_packages -y
+help_setup(){
+    echo "use env-install to setup environment"
+    echo "use install keyword as args to install setup"
+    echo "use remove keyword as args to remove setup"
+}
+
+env_install(){
+    [ -d ~/.oh-my-zsh/ ] && echo ".oh-my-zsh already exists" || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    [ -d ~/.fzf ] && echo ".fzf already exists" || git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    [ -f  $PREFIX/bin/fzf ] || ~/.fzf/install
+}
+
+install(){
+    [  -f  ~/.zshrc ] && rm ~/.zshrc
+    [ -d  ~/.termux  ] && rm .termux -rf
+    stow -t $HOME -S */
+    echo "added all config file in root dir"
 }
 
 case "$1" in
-    "packages") installPackages ;;
-    "env")
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-        git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-        ~/.fzf/install ;;
-    *) echo "USE packages to install packages"
-        echo "USE env to install workspace";;
+    "env-install")
+        env_install ;;
+    "install")
+        install ;;
+    "remove")
+        stow -t $HOME -D */ ;;
+    *) help_setup
 esac
