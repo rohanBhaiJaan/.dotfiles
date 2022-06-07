b329a7277c06115d4935ddc56a85c51de29e1d50
rohanBhaiJaan <rohanprogrammer@gmail.com>
2022-06-07 18:06:42 +0530

    now user can open his fav editor while using tmux-multiplexer

--- local/.local/bin/tmux-multiplexer.sh
+++ local/.local/bin/tmux-multiplexer.sh
@@ -1,10 +1,32 @@
 #!/data/data/com.termux/files/usr/bin/env bash
 # syntax: tmux-multiplexer
 
-if [ $# -gt 1 ]; then
-    echo "TOO MANY ARGS"
-    exit
-fi
+help_menu(){
+    echo "-name will rename the session name default is folder name"
+    echo "-editor will open your text editor default is nvim"
+    echo "-help will provide you help menu"
+}
+
+while test $# -gt 0; do
+    case "$1" in 
+        -help)
+            help_menu;
+            exit 0;;
+        -editor)
+            shift 
+            editor="$1"
+            shift
+            ;;
+        -name)
+            shift
+            session_name="$1"
+            shift
+            ;;
+        *)
+            help_menu
+            exit -1;;
+    esac
+done
 
 set -e
 
@@ -23,13 +45,17 @@ paths+=( ${additional_folders[@]} )
 project_path=$( echo "${paths[@]}" | tr ' ' '\n' | fzf)
 folder_name_arr=($( echo $project_path | tr '/' ' ' ))
 
-if [ $# -eq 1 ]; then
-    session_name=$1
-else
+if [ ! "$session_name" ]; then
     session_name="$(echo ${folder_name_arr[-1]} | sed "s/\W/_/g" )"
 fi
 
+if [ ! "$editor" ]; then
+    editor="nvim"
+fi
+
+echo "$editor $session_name"
+
 # setting session
 tmux new-session -s $session_name -c "$project_path" -n term -d
-tmux new-window -n editor -c "$project_path" "nvim ."
+tmux new-window -n editor -c "$project_path" "$editor ."
 tmux attach -t $session_name
